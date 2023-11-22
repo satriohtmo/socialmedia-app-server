ec2102c3a760457322443124137636fae481096f
Satrio Hutomo <strhtmo17@gmail.com>
2023-11-23 03:25:37 +0700

    config change to connect db

--- config/config.js
+++ config/config.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const url = process.env.DB_URL;
 
 const sequelize = new Sequelize(url, {
-  dialect: "postgres",
+  define: {
+    timestamps: false,
+  },
 });
 
 module.exports = sequelize;
