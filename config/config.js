71891846a607cd9ba9f56565c511659e03bd6b65
Satrio Hutomo <strhtmo17@gmail.com>
2023-11-22 21:25:27 +0700

    update vercel

--- config/config.js
+++ config/config.js
@@ -3,7 +3,17 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.PG_URL + "?sslmode=require",
+  connectionString:
+    process.env.NODE_ENV === "production"
+      ? process.env.DATABASE_URL + "?sslmode=require"
+      : {
+          user: process.env.DB_USER || "postgres",
+          password: process.env.DB_PASSWORD || "Bulldogz",
+          database: process.env.DB_NAME || "sosmed",
+          host: process.env.DB_HOST || "127.0.0.1",
+          port: process.env.DB_PORT || 5432, // Your PostgreSQL port
+          ssl: process.env.NODE_ENV !== "development", // Enable SSL for non-development environments
+        },
 });
 
 pool.connect((err) => {
