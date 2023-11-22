b65650915fe9f88afc6155822dd956fb47b7095c
Satrio Hutomo <strhtmo17@gmail.com>
2023-11-23 03:19:12 +0700

    change config

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ sequelize
   .catch((err) => {
     console.error("Unable to connect to the database:", err);
   });
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 const url = process.env.DB_URL;
-console.log(url);
 
 const sequelize = new Sequelize(url, {
   dialect: "postgres",
