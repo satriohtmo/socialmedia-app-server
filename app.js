1362a38dff4a091d4433813910507def0579ff3c
Satrio Hutomo <strhtmo17@gmail.com>
2023-11-23 02:21:55 +0700

    hotfix/config

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const sequelize = require("./config/config"); // Import your Sequelize configuration
 const routes = require("./routes");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -12,6 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(routes);
 app.use(errHandler);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection to the database has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- config/config.js
+++ config/config.js
@@ -1,24 +1,10 @@
+const { Sequelize } = require("sequelize");
 require("dotenv").config();
+const url = process.env.DB_URL;
+console.log(url);
 
-const { Pool } = require("pg");
-
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production"
-      ? process.env.PG_URL + "?sslmode=require"
-      : {
-          user: process.env.DB_USER || "postgres",
-          password: process.env.DB_PASSWORD || "Bulldogz",
-          database: process.env.DB_NAME || "sosmed",
-          host: process.env.DB_HOST || "127.0.0.1",
-          port: process.env.DB_PORT || 5432, // Your PostgreSQL port
-          ssl: process.env.NODE_ENV !== "development", // Enable SSL for non-development environments
-        },
+const sequelize = new Sequelize(url, {
+  dialect: "postgres",
 });
 
-pool.connect((err) => {
-  if (err) throw err;
-  console.log("connect to pgb database");
-});
-
-module.exports = pool;
+module.exports = sequelize;
